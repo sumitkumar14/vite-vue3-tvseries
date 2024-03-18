90ef52ea218e5f8a0a49eb8f8321e31b7835cc6c
Sumit Kumar <sumitkumar14695@gmail.com>
2024-03-19 03:20:52 +0530

    scroll behaviour top fixed

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,11 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
+  routes,
 })
 
 export default router

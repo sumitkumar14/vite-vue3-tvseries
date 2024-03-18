cf245989d36152ca7abe0f7ab13ab45a7d704cc3
Sumit Kumar <sumitkumar14695@gmail.com>
2024-03-19 03:33:02 +0530

    click on tvshows

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <h2><v-icon class="mb-1 mr-1">mdi-television</v-icon>TV Shows{{tvSeriesName?` / ${tvSeriesName}`:''}}</h2>
-      </div>
-    </v-app-bar>
+    <Toolbar/>
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
 <script setup>
-import { useTvStore } from '@/stores/tvStore'
-import { computed } from 'vue';
-const store = useTvStore();
-const tvSeriesName = computed(()=>store.tvSeriesName);
+import  Toolbar from '@/components/Toolbar.vue'
 </script>
 
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app-bar app color="primary" dark>
+        <div class="d-flex align-center">
+            <h2 style="cursor: pointer;" @click="back()"><v-icon class="mb-1 mr-1">mdi-television</v-icon>TV Shows{{tvSeriesName?` /
+                ${tvSeriesName}`:''}}</h2>
+        </div>
+    </v-app-bar>
+</template>
+<script setup>
+import { useRouter } from 'vue-router'
+import { useTvStore } from '@/stores/tvStore'
+import { computed } from 'vue';
+
+const store = useTvStore();
+const router = useRouter();
+const tvSeriesName = computed(() => store.tvSeriesName);
+function back() {
+    router.push({ path: "/" });
+}
+</script>
